c5d04231bef3ae85a737c896bab244986aec4cf1
Douglas Martin <dkmartin@regenstrief.org>
2014-07-14 23:43:15 -0400

    Remove old license language.

--- org.carewebframework.api/src/test/java/org/carewebframework/api/security/MockSecurityService.java
+++ org.carewebframework.api/src/test/java/org/carewebframework/api/security/MockSecurityService.java
@@ -9,18 +9,6 @@
  */
 package org.carewebframework.api.security;
 
-/**
- * The contents of this file are subject to the Regenstrief Public License
- * Version 1.0 (the "License"); you may not use this file except in compliance with the License.
- * Please contact Regenstrief Institute if you would like to obtain a copy of the license.
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) Regenstrief Institute.  All Rights Reserved.
- */
 import org.carewebframework.api.domain.IDomainObject;
 
 /**
