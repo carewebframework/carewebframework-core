7db29d0087fcaf07cd5d6d35ca7af78e78c2570c
Douglas Martin <dkmartin@regenstrief.org>
2014-12-17 23:41:14 -0500

    Remove unused code.

--- org.carewebframework.ui/src/main/resources/web/js/cwf/ext/mold/SplitterView.js
+++ org.carewebframework.ui/src/main/resources/web/js/cwf/ext/mold/SplitterView.js
@@ -2,7 +2,6 @@
  * Mold: SplitterView.js
  */
 function (out) {
-	var zcls = this.getZclass();
 	out.push('<div', this.domAttrs_(), '>');
 	for (var child = this.firstChild; child; child = child.nextSibling)
 		child.redraw(out);
