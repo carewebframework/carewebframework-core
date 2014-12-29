87852e78c9117917f95368acce1a4071405186a0
Douglas Martin <dkmartin@regenstrief.org>
2014-12-29 14:09:54 -0500

    Improve error reporting for unsupported help format.

--- org.carewebframework.help-parent/org.carewebframework.help.core/src/main/java/org/carewebframework/help/HelpSetFactory.java
+++ org.carewebframework.help-parent/org.carewebframework.help.core/src/main/java/org/carewebframework/help/HelpSetFactory.java
@@ -1,6 +1,6 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
  * http://mozilla.org/MPL/2.0/.
  * 
  * This Source Code Form is also subject to the terms of the Health-Related Additional
@@ -68,7 +68,7 @@ public class HelpSetFactory {
             return ctor.newInstance(url);
         } catch (Exception e) {
             log.error("Error creating help set for " + url, e);
-            return null;
+            throw new RuntimeException(e);
         }
     }
     
