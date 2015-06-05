5a313ef2423d2e0feb72a727345bf2a162d8cbb3
Douglas Martin <dkmartin@regenstrief.org>
2015-06-05 15:19:16 -0400

    RowComparator auto wiring methods now support tri-state sorting toggles.

--- org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/zk/RowComparator.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/zk/RowComparator.java
@@ -20,6 +20,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.event.SortEvent;
 import org.zkoss.zul.Column;
 import org.zkoss.zul.Grid;
 import org.zkoss.zul.Listbox;
@@ -29,63 +32,10 @@ import org.zkoss.zul.Treecol;
 import org.zkoss.zul.impl.HeaderElement;
 
 /**
- * Serves as a generic comparator for list and grid displays.
+ * Serves as a generic comparator for list and grid displays. Includes a number of static methods
+ * for auto-wiring comparators into sortable header elements.
  */
 public class RowComparator implements Comparator<Object>, Serializable {
-    
-    private static final Log log = LogFactory.getLog(RowComparator.class);
-    
-    private static final long serialVersionUID = 1L;
-    
-    private final boolean _asc;
-    
-    private final String _getter;
-    
-    private final Comparator<Object> _customComparator;
-    
-    /**
-     * Automatically wires column headers to generic comparators.
-     * <p>
-     * The getter method for each header element is derived either from a custom attribute named
-     * "getter" or, absent that, from the element's id. This value may either be a property name or
-     * the name of the getter method itself. This method is used when comparing values across rows
-     * under that header. If getter is specified for a header, no comparator is generated for that
-     * header.
-     * <p>
-     * Custom comparators may be specified in a custom attribute named "comparator" on the header
-     * element. This attribute may be an object instance that implements the Comparator interface or
-     * the name of a class that implements the Comparator interface.
-     * 
-     * @param headers List of headers (of type Column, Listheader, or Treecol).
-     */
-    public static void autowireColumnComparators(List<Component> headers) {
-        for (Component cmp : headers) {
-            if (cmp instanceof HeaderElement) {
-                String getter = getterMethod(cmp);
-                
-                if (getter == null) {
-                    continue;
-                }
-                
-                Comparator<?> comparator = getComparator(cmp);
-                RowComparator asc = new RowComparator(true, getter, comparator);
-                RowComparator dsc = new RowComparator(false, getter, comparator);
-                
-                if (cmp instanceof Column) {
-                    ((Column) cmp).setSortAscending(asc);
-                    ((Column) cmp).setSortDescending(dsc);
-                } else if (cmp instanceof Listheader) {
-                    ((Listheader) cmp).setSortAscending(asc);
-                    ((Listheader) cmp).setSortDescending(dsc);
-                } else if (cmp instanceof Treecol) {
-                    ((Treecol) cmp).setSortAscending(asc);
-                    ((Treecol) cmp).setSortDescending(dsc);
-                }
-            } else {
-                throw new IllegalArgumentException("Component not a supported type: " + cmp.getClass().getName());
-            }
-        }
-    }
     
     /**
      * Convenience method for auto-wiring list box headers.
@@ -130,6 +80,195 @@ public class RowComparator implements Comparator<Object>, Serializable {
             autowireColumnComparators(parent.getChildren());
         }
     }
+    
+    /**
+     * Automatically wires column headers to generic comparators.
+     * <p>
+     * The getter method for each header element is derived either from a custom attribute named
+     * "getter" or, absent that, from the element's id. This value may either be a property name or
+     * the name of the getter method itself. This method is used when comparing values across rows
+     * under that header. If getter is specified for a header, no comparator is generated for that
+     * header.
+     * <p>
+     * Custom comparators may be specified in a custom attribute named "comparator" on the header
+     * element. This attribute may be an object instance that implements the Comparator interface or
+     * the name of a class that implements the Comparator interface.
+     * <p>
+     * Finally, if a column has been marked with the custom attribute named "natural", that column
+     * will be used to establish the "natural" ordering of the model. The value of the attribute may
+     * be either "ascending" or "descending" (default is "ascending" if any other value is
+     * specified) to indicate the direction of the natural ordering. When this attribute is present,
+     * the sort toggle becomes tri-state, toggling from "ascending" to "descending" to "natural" in
+     * that order.
+     * 
+     * @param headers List of headers (of type Column, Listheader, or Treecol).
+     */
+    public static void autowireColumnComparators(List<Component> headers) {
+        Component defaultSortHeader = getDefaultSortHeader(headers);
+        SortListener sortListener = defaultSortHeader == null ? null : new SortListener(defaultSortHeader);
+        
+        for (Component cmp : headers) {
+            if (cmp instanceof HeaderElement) {
+                String getter = getGetter(cmp);
+                
+                if (getter == null) {
+                    continue;
+                }
+                
+                Comparator<?> comparator = getCustomComparator(cmp);
+                RowComparator asc = new RowComparator(true, getter, comparator);
+                RowComparator dsc = new RowComparator(false, getter, comparator);
+                
+                if (sortListener != null) {
+                    cmp.addEventListener(Events.ON_SORT, sortListener);
+                }
+                
+                if (cmp instanceof Column) {
+                    ((Column) cmp).setSortAscending(asc);
+                    ((Column) cmp).setSortDescending(dsc);
+                } else if (cmp instanceof Listheader) {
+                    ((Listheader) cmp).setSortAscending(asc);
+                    ((Listheader) cmp).setSortDescending(dsc);
+                } else if (cmp instanceof Treecol) {
+                    ((Treecol) cmp).setSortAscending(asc);
+                    ((Treecol) cmp).setSortDescending(dsc);
+                }
+            } else {
+                throw new IllegalArgumentException("Component not a supported type: " + cmp.getClass().getName());
+            }
+        }
+    }
+    
+    /**
+     * Sort event listener that intercepts sort events to implement tri-state sort toggling.
+     */
+    private static class SortListener implements EventListener<SortEvent> {
+        
+        private final Component sortDefault;
+        
+        public SortListener(Component sortDefault) {
+            this.sortDefault = sortDefault;
+        }
+        
+        @Override
+        public void onEvent(SortEvent event) throws Exception {
+            Component cmp = event.getTarget();
+            
+            if (cmp instanceof Column) {
+                Column header = (Column) cmp;
+                
+                if ("descending".equals(header.getSortDirection())) {
+                    header.setSortDirection("natural");
+                    header = (Column) sortDefault;
+                    header.sort(event.isAscending());
+                    header.setSortDirection("natural");
+                    event.stopPropagation();
+                }
+            } else if (cmp instanceof Listheader) {
+                Listheader header = (Listheader) cmp;
+                
+                if ("descending".equals(header.getSortDirection())) {
+                    header.setSortDirection("natural");
+                    header = (Listheader) sortDefault;
+                    header.sort(event.isAscending());
+                    header.setSortDirection("natural");
+                    event.stopPropagation();
+                }
+            } else if (cmp instanceof Treecol) {
+                Treecol header = (Treecol) cmp;
+                
+                if ("descending".equals(header.getSortDirection())) {
+                    header.setSortDirection("natural");
+                    header = (Treecol) sortDefault;
+                    header.sort(event.isAscending());
+                    header.setSortDirection("natural");
+                    event.stopPropagation();
+                }
+            }
+        }
+    };
+    
+    /**
+     * If the component has a custom attribute named "getter", that value is returned. Otherwise,
+     * the component's id is assumed to be either a property name or the getter method name. If the
+     * former, assumes the getter method is getXxxx.
+     * 
+     * @param component Component used to derive getter method.
+     * @return Null if the component has no id and no getter attribute, or has a ZK-generated id.
+     *         Otherwise, if the id is prefixed with a standard getter prefix ("get", "has", "is"),
+     *         it is assumed to be the name of the getter method. Lacking such a prefix, a prefix of
+     *         "get" is prepended to the case-adjusted id to obtain the getter method.
+     */
+    private static String getGetter(Component component) {
+        if (component.hasAttribute("getter")) {
+            return (String) component.getAttribute("getter");
+        }
+        
+        String id = component.getId();
+        
+        if (id == null || id.isEmpty() || id.startsWith("z")) {
+            return null;
+        }
+        
+        String lc = id.toLowerCase();
+        
+        if (lc.startsWith("is") || lc.startsWith("has") || lc.startsWith("get")) {
+            return StringUtils.uncapitalize(id); // is the getter name
+        }
+        return "get".concat(StringUtils.capitalize(id)); // infer getter name
+    }
+    
+    /**
+     * Returns an optional custom comparator from a component. A custom comparator is specific in
+     * the custom attribute named "comparator" and may be an instance of a Comparator or the name of
+     * a class implementing Comparator.
+     * 
+     * @param component Component whose custom comparator is sought.
+     * @return A custom comparator, if any.
+     */
+    private static Comparator<?> getCustomComparator(Component component) {
+        Object cmpr = component.getAttribute("comparator");
+        
+        if (cmpr instanceof String) {
+            try {
+                Class<?> clazz = ClassUtils.getClass((String) cmpr);
+                
+                if (Comparator.class.isAssignableFrom(clazz)) {
+                    cmpr = clazz.newInstance();
+                }
+            } catch (Exception e) {
+                cmpr = null;
+            }
+        }
+        
+        return cmpr instanceof Comparator ? (Comparator<?>) cmpr : null;
+    }
+    
+    /**
+     * Gets the header element marked as governing the "natural" sort order of the model.
+     * 
+     * @param headers The list of headers to search.
+     * @return The header that has been designated for default sorting, or null if none.
+     */
+    private static Component getDefaultSortHeader(List<Component> headers) {
+        for (Component header : headers) {
+            if (header.hasAttribute("natural")) {
+                return header;
+            }
+        }
+        
+        return null;
+    }
+    
+    private static final Log log = LogFactory.getLog(RowComparator.class);
+    
+    private static final long serialVersionUID = 1L;
+    
+    private final boolean _asc;
+    
+    private final String _getter;
+    
+    private final Comparator<Object> _customComparator;
     
     /**
      * Constructs a row comparator.
@@ -202,61 +341,5 @@ public class RowComparator implements Comparator<Object>, Serializable {
             log.error(e.getMessage(), e);
             return null;
         }
-    }
-    
-    /**
-     * If the component has a custom attribute named "getter", that value is returned. Otherwise,
-     * the component's id is assumed to be either a property name or the getter method name. If the
-     * former, assumes the getter method is getXxxx.
-     * 
-     * @param component Component used to derive getter method.
-     * @return Null if the component has no id and no getter attribute, or has a ZK-generated id.
-     *         Otherwise, if the id is prefixed with a standard getter prefix ("get", "has", "is"),
-     *         it is assumed to be the name of the getter method. Lacking such a prefix, a prefix of
-     *         "get" is prepended to the case-adjusted id to obtain the getter method.
-     */
-    private static String getterMethod(Component component) {
-        if (component.hasAttribute("getter")) {
-            return (String) component.getAttribute("getter");
-        }
-        
-        String id = component.getId();
-        
-        if (id == null || id.isEmpty() || id.startsWith("z")) {
-            return null;
-        }
-        
-        String lc = id.toLowerCase();
-        
-        if (lc.startsWith("is") || lc.startsWith("has") || lc.startsWith("get")) {
-            return StringUtils.uncapitalize(id); // is the getter name
-        }
-        return "get".concat(StringUtils.capitalize(id)); // infer getter name
-    }
-    
-    /**
-     * Returns an optional custom comparator from a component. A custom comparator is specific in
-     * the custom attribute named "comparator" and may be an instance of a Comparator or the name of
-     * a class implementing Comparator.
-     * 
-     * @param component Component whose custom comparator is sought.
-     * @return A custom comparator, if any.
-     */
-    private static Comparator<?> getComparator(Component component) {
-        Object cmpr = component.getAttribute("comparator");
-        
-        if (cmpr instanceof String) {
-            try {
-                Class<?> clazz = ClassUtils.getClass((String) cmpr);
-                
-                if (Comparator.class.isAssignableFrom(clazz)) {
-                    cmpr = clazz.newInstance();
-                }
-            } catch (Exception e) {
-                cmpr = null;
-            }
-        }
-        
-        return cmpr instanceof Comparator ? (Comparator<?>) cmpr : null;
     }
 }
