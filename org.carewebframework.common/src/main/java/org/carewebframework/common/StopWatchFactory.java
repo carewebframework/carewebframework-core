44ecaeb97c585217eba08543ee98940bc57c0ab3
Douglas Martin <dkmartin@regenstrief.org>
2016-02-01 10:27:33 -0500

    Provide a friendlier error message if no stopwatch factory has been registered.

--- org.carewebframework.common/src/main/java/org/carewebframework/common/StopWatchFactory.java
+++ org.carewebframework.common/src/main/java/org/carewebframework/common/StopWatchFactory.java
@@ -1,6 +1,6 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
  * http://mozilla.org/MPL/2.0/.
  * 
  * This Source Code Form is also subject to the terms of the Health-Related Additional
@@ -64,8 +64,12 @@ public class StopWatchFactory {
      *         initialized.
      */
     public static IStopWatch create() {
+        if (factory == null) {
+            throw new IllegalStateException("No stopwatch factory has been registered.");
+        }
+        
         try {
-            return factory == null ? null : factory.clazz.newInstance();
+            return factory.clazz.newInstance();
         } catch (Exception e) {
             throw new RuntimeException("Could not create stopwatch instance.", e);
         }
