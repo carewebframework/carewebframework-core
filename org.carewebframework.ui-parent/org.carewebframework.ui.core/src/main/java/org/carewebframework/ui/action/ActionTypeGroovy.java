415cdd7e46192c60f7d5049bb7ccf77ca1dea70e
Douglas Martin <dkmartin@regenstrief.org>
2016-11-30 13:51:59 -0500

    Updates.

--- org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/action/ActionTypeGroovy.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/action/ActionTypeGroovy.java
@@ -1,31 +1,23 @@
 package org.carewebframework.ui.action;
 
-import java.lang.reflect.Method;
-
 import org.carewebframework.common.MiscUtil;
 
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+
 /**
- * Action type is a Groovy script. Groovy shell classes are loaded on demand.
+ * Action type is a Groovy script.
  */
 public class ActionTypeGroovy extends ActionTypeBase {
     
     private static volatile boolean initialized;
     
-    private static Object shell;
-    
-    private static Method parseMethod;
-    
-    private static Class<?> scriptClass;
-    
-    private static Method runMethod;
+    private static GroovyShell shell;
     
     private static synchronized void initialize() {
         if (!initialized) {
             try {
-                shell = Class.forName("groovy.lang.GroovyShell").newInstance();
-                parseMethod = shell.getClass().getMethod("parse", String.class);
-                scriptClass = Class.forName("groovy.lang.Script");
-                runMethod = scriptClass.getMethod("run");
+                shell = new GroovyShell();
                 initialized = true;
             } catch (Exception e) {
                 throw MiscUtil.toUnchecked(e);
@@ -38,11 +30,11 @@ public class ActionTypeGroovy extends ActionTypeBase {
     }
     
     @Override
-    public Object parse(String script) {
+    public Script parse(String script) {
         init();
         
         try {
-            return parseMethod.invoke(shell, stripPrefix(script));
+            return shell.parse(stripPrefix(script));
         } catch (Exception e) {
             throw MiscUtil.toUnchecked(e);
         }
@@ -51,12 +43,7 @@ public class ActionTypeGroovy extends ActionTypeBase {
     @Override
     public void execute(Object script) {
         init();
-        
-        try {
-            runMethod.invoke(script);
-        } catch (Exception e) {
-            throw MiscUtil.toUnchecked(e);
-        }
+        ((Script) script).run();
     }
     
     private void init() {
