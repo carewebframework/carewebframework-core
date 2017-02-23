00d639acc589e509c5b5e49e361c95cf99f75673
Douglas Martin <dkmartin@regenstrief.org>
2017-02-23 10:13:56 -0500

    Updates.

--- org.carewebframework.plugin-parent/org.carewebframework.plugin.infopanel/src/main/java/org/carewebframework/plugin/infopanel/controller/DropContainer.java
+++ org.carewebframework.plugin-parent/org.carewebframework.plugin.infopanel/src/main/java/org/carewebframework/plugin/infopanel/controller/DropContainer.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -42,16 +42,16 @@ import org.carewebframework.web.page.PageUtil;
  * Container for receiving components rendered by drop renderer.
  */
 public class DropContainer extends Window implements IActionTarget {
-    
+
     private static final String SCLASS = "cwf-infopanel-container";
-    
+
     private static final String TEMPLATE = "web/org/carewebframework/plugin/infopanel/dropContainer.cwf";
-    
+
     private List<ActionListener> actionListeners;
-    
+
     /**
      * Renders a droppedItem in a container.
-     * 
+     *
      * @param dropRoot The root component that will host the container.
      * @param droppedItem The item being dropped.
      * @return The container hosting the rendered item. If the droppedItem was previously rendered,
@@ -61,18 +61,18 @@ public class DropContainer extends Window implements IActionTarget {
      */
     public static DropContainer render(BaseComponent dropRoot, BaseComponent droppedItem) {
         IDropRenderer dropRenderer = DropUtil.getDropRenderer(droppedItem);
-        
+
         if (dropRenderer == null || !dropRenderer.isEnabled()) {
             return null;
         }
-        
+
         BaseComponent renderedItem = dropRenderer.renderDroppedItem(droppedItem);
         DropContainer dropContainer = null;
-        
+
         if (renderedItem != null) {
             String title = dropRenderer.getDisplayText(droppedItem);
             dropContainer = renderedItem.getAncestor(DropContainer.class);
-            
+
             if (dropContainer != null) {
                 dropContainer.setTitle(title);
                 dropContainer.moveToTop(dropRoot);
@@ -81,17 +81,18 @@ public class DropContainer extends Window implements IActionTarget {
                     InfoPanelService.getActionListeners(droppedItem));
             }
         }
-        
+
         return dropContainer;
     }
-    
+
     /**
      * Creates a new container for the contents to be rendered by the drop provider.
-     * 
+     *
      * @param dropRoot The root component that will host the container.
      * @param cmpt BaseComponent rendered by the drop renderer.
      * @param title Title to be associated with the container.
      * @param actionListeners Listeners to be bound to the drop container (optional).
+     * @return The drop container.
      */
     private static DropContainer create(BaseComponent dropRoot, BaseComponent cmpt, String title,
                                         List<ActionListener> actionListeners) {
@@ -105,10 +106,10 @@ public class DropContainer extends Window implements IActionTarget {
         ActionListener.bindActionListeners(dc, actionListeners);
         return dc;
     }
-    
+
     /**
      * Perform the specified action on the drop container.
-     * 
+     *
      * @param action An action.
      */
     @Override
@@ -117,39 +118,39 @@ public class DropContainer extends Window implements IActionTarget {
             case REMOVE:
                 close();
                 break;
-            
+
             case HIDE:
                 setVisible(false);
                 break;
-            
+
             case SHOW:
                 setVisible(true);
                 break;
-            
+
             case COLLAPSE:
                 setSize(Size.MINIMIZED);
                 break;
-            
+
             case EXPAND:
                 setSize(Size.NORMAL);
                 break;
-            
+
             case TOP:
                 moveToTop();
                 break;
         }
     }
-    
+
     /**
      * Moves an existing entry to the beginning of the stream.
      */
     public void moveToTop() {
         moveToTop(getParent());
     }
-    
+
     /**
      * Moves this entry to the beginning of the stream.
-     * 
+     *
      * @param dropRoot Parent
      */
     public void moveToTop(BaseComponent dropRoot) {
@@ -158,21 +159,21 @@ public class DropContainer extends Window implements IActionTarget {
             this.scrollIntoView(false);
         }
     }
-    
+
     /**
      * Supports dragging drop container to a new position in the stream.
-     * 
+     *
      * @param event The drop event.
      */
     @EventHandler("drop")
     private void onDrop(DropEvent event) {
         BaseComponent dragged = event.getRelatedTarget();
-        
+
         if (dragged instanceof DropContainer) {
             getParent().addChild(dragged, this);
         }
     }
-    
+
     @Override
     public void destroy() {
         ActionListener.unbindActionListeners(this, actionListeners);
--- org.carewebframework.shell/src/main/java/org/carewebframework/shell/elements/UIElementTabView.java
+++ org.carewebframework.shell/src/main/java/org/carewebframework/shell/elements/UIElementTabView.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -54,13 +54,13 @@ public class UIElementTabView extends UIElementBase {
         setOuterComponent(tabview);
         tabview.addClass("cwf-tabview");
         tabview.addEventListener(ChangeEvent.TYPE, (event) -> {
-            setActivePane((UIElementTabPane) getAssociatedUIElement(event.getTarget()));
+            setActivePane((UIElementTabPane) getAssociatedUIElement(tabview.getSelectedTab()));
         });
     }
     
     /**
      * Sets the orientation which can be horizontal or vertical.
-     * 
+     *
      * @param orientation Orientation setting.
      */
     public void setOrientation(String orientation) {
@@ -69,7 +69,7 @@ public class UIElementTabView extends UIElementBase {
     
     /**
      * Returns the orientation (horizontal, vertical, top, or bottom).
-     * 
+     *
      * @return Orientation setting.
      */
     public String getOrientation() {
@@ -88,7 +88,7 @@ public class UIElementTabView extends UIElementBase {
     
     /**
      * Sets the active (visible) pane.
-     * 
+     *
      * @param pane The pane to become active.
      */
     protected void setActivePane(UIElementTabPane pane) {
