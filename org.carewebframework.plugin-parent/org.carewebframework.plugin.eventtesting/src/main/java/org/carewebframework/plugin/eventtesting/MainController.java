d6d23d187842fa815dd589abed40bc8b67831b8d
Douglas Martin <dkmartin@regenstrief.org>
2017-02-23 08:29:51 -0500

    Updates.

--- org.carewebframework.plugin-parent/org.carewebframework.plugin.eventtesting/src/main/java/org/carewebframework/plugin/eventtesting/MainController.java
+++ org.carewebframework.plugin-parent/org.carewebframework.plugin.eventtesting/src/main/java/org/carewebframework/plugin/eventtesting/MainController.java
@@ -53,115 +53,115 @@ import org.carewebframework.web.event.ChangeEvent;
  * Plug-in to test remote events.
  */
 public class MainController extends PluginController implements IGenericEvent<Object> {
-
+    
     @WiredComponent
     private Textbox tboxEventName;
-
+    
     @WiredComponent
     private Textbox tboxEventRecipients;
-
+    
     @WiredComponent
     private Memobox tboxEventData;
-
+    
     @WiredComponent
     private Memobox tboxEventResults;
-
+    
     @WiredComponent
     private Textbox tboxNewEvent;
-
+    
     @WiredComponent
     private Listbox lboxEventList;
-
+    
     @WiredComponent
     private Checkbox chkAutoGenerate;
-
+    
     @WiredComponent
     private Checkbox chkScrollLock;
-
+    
     @WiredComponent
     private Label lblInfo;
-
+    
     private final IEventManager eventManager = EventManager.getInstance();
-
+    
     private int messageCount;
-
+    
     @EventHandler(value = "click", target = "btnSend")
     private void onClick$btnSend() {
         messageCount++;
-
+        
         if (chkAutoGenerate.isChecked()) {
             tboxEventData.setValue("Sending test event #" + messageCount);
         }
-
+        
         eventManager.fireRemoteEvent(tboxEventName.getValue(), tboxEventData.getValue(),
             parseRecipients(tboxEventRecipients.getValue()));
         info("Fired", tboxEventName.getValue());
     }
-
+    
     @EventHandler(value = "click", target = "btnReset")
     private void onClick$btnReset() {
         tboxEventName.setValue("");
         tboxEventRecipients.setValue("");
         tboxEventData.setValue("");
     }
-
+    
     @EventHandler(value = "click", target = "btnPing")
     private void onClick$btnPing() {
         EventUtil.ping("PING.RESPONSE", null);
     }
-
+    
     @EventHandler(value = "click", target = "btnClear")
     private void onClick$btnClear() {
         tboxEventResults.setValue("");
     }
-
+    
     @EventHandler(value = "click", target = "btnNewEvent")
     private void onClick$btnNewEvent() {
         String eventName = tboxNewEvent.getValue().trim();
-
+        
         if (!StringUtils.isEmpty(eventName) && !containsEvent(eventName)) {
             Listitem item = new Listitem();
             item.setLabel(eventName);
             lboxEventList.addChild(item);
         }
-
+        
         tboxNewEvent.setValue("");
     }
-
+    
     private Recipient[] parseRecipients(String text) {
         if (text == null || text.isEmpty()) {
             return null;
         }
-
+        
         List<Recipient> recipients = new ArrayList<>();
-
+        
         for (String recip : text.split("\\,")) {
             String[] pcs = recip.split("\\:", 2);
-
+            
             if (pcs.length == 2) {
                 RecipientType type = RecipientType.valueOf(pcs[0].trim().toUpperCase());
                 recipients.add(new Recipient(type, pcs[1]));
             }
         }
-
+        
         return recipients.isEmpty() ? null : (Recipient[]) recipients.toArray();
     }
-
+    
     private boolean containsEvent(String eventName) {
         for (Object object : lboxEventList.getChildren()) {
             if (((Listitem) object).getLabel().equals(eventName)) {
                 return true;
             }
         }
-
+        
         return false;
     }
-
+    
     @EventHandler(value = "change", target = "lboxEventList")
     private void onChange$lboxEventList(ChangeEvent event) {
-        Listitem item = (Listitem) event.getTarget();
+        Listitem item = (Listitem) event.getValue();
         String eventName = item.getLabel();
-
+        
         if (item.isSelected()) {
             eventManager.subscribe(eventName, this);
             info("Subscribed to", eventName);
@@ -170,29 +170,29 @@ public class MainController extends PluginController implements IGenericEvent<Ob
             info("Unsubscribed from", eventName);
         }
     }
-
+    
     private void info(String action, String eventName) {
         lblInfo.setLabel(action + " '" + eventName + " ' event.");
     }
-
+    
     @Override
     public void eventCallback(String eventName, Object eventData) {
         String s = tboxEventResults.getValue();
-
+        
         if (!(eventData instanceof String)) {
             try {
                 eventData = JSONUtil.serialize(eventData, true);
             } catch (Exception e) {}
         }
-
+        
         s += "\n\n" + eventName + ":\n" + eventData;
         tboxEventResults.setValue(s);
         info("Received", eventName);
-
+        
         if (!chkScrollLock.isChecked()) {
             String js = StrUtil.formatMessage("$('#%1$s').scrollTop($('#%1$s')[0].scrollHeight);", tboxEventResults.getId());
             ClientUtil.eval(js);
         }
     }
-
+    
 }
