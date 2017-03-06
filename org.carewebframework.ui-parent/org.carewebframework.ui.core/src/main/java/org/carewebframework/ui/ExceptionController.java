8af3153352a3fde0b6da10d3dd75f61bfed35b9d
Douglas Martin <dkmartin@regenstrief.org>
2017-03-06 13:33:59 -0500

    Updates

--- org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/ExceptionController.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/ExceptionController.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -48,43 +48,38 @@ import org.carewebframework.web.component.Window;
 import org.carewebframework.web.core.RequestUtil;
 import org.carewebframework.web.event.ClickEvent;
 import org.carewebframework.web.event.EventUtil;
-import org.springframework.core.ErrorCoded;
 import org.springframework.core.NestedCheckedException;
 import org.springframework.core.NestedRuntimeException;
 import org.springframework.web.util.WebUtils;
 
 /**
- * Controller to handle exceptions caught by the framework. 
- * This class logs to our our logging appender with the
- * name defined by constant {@link Constants#EXCEPTION_LOG}.
+ * Controller to handle exceptions caught by the framework. This class logs to our our logging
+ * appender with the name defined by constant {@link Constants#EXCEPTION_LOG}.
  */
 public class ExceptionController implements IAutoWired {
-    
+
     private static final Log log = LogFactory.getLog(ExceptionController.class);//TODO Constants.EXCEPTION_LOG);
-    
+
     private Window root;
-    
+
     @WiredComponent
     private Label lblExceptionClass;
-    
+
     @WiredComponent
     private Label lblMessage;
-    
+
     @WiredComponent
     private Label lblStatusCode;
-    
-    @WiredComponent
-    private Label lblCode;
-    
+
     @WiredComponent
     private Textbox txtStackTrace;
-    
+
     @WiredComponent
     private BaseUIComponent detail;
-    
+
     @WiredComponent
     private Button btnDetail;
-    
+
     /**
      * Populate the display with information from the current execution.
      */
@@ -93,22 +88,22 @@ public class ExceptionController implements IAutoWired {
         ClientUtil.busy(null, null);
         this.root = comp.getAncestor(Window.class);
         HttpServletRequest req = RequestUtil.getRequest();
-        
+
         Class<?> errClass = (Class<?>) req.getAttribute(WebUtils.ERROR_EXCEPTION_TYPE_ATTRIBUTE);
         String errMsg = (String) req.getAttribute(WebUtils.ERROR_MESSAGE_ATTRIBUTE);
         Throwable err = (Throwable) req.getAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE);
-        
+
         String errReqURI = (String) req.getAttribute(WebUtils.ERROR_REQUEST_URI_ATTRIBUTE);
         String errServletName = (String) req.getAttribute(WebUtils.ERROR_SERVLET_NAME_ATTRIBUTE);
         Integer errStatusCode = (Integer) req.getAttribute(WebUtils.ERROR_STATUS_CODE_ATTRIBUTE);
-        
+
         String throwableContext = null;
-        
+
         if (err != null) {
             if (err instanceof IThrowableContext) {
                 throwableContext = ((IThrowableContext) err).getThrowableContext();
             }
-            
+
             //stack trace info
             if (err instanceof NestedCheckedException) {
                 err = ((NestedCheckedException) err).getMostSpecificCause();
@@ -120,40 +115,35 @@ public class ExceptionController implements IAutoWired {
             errClass = errClass == null ? err.getClass() : errClass;
             errMsg = StringUtils.trimToNull(errMsg) == null ? err.getMessage() : errMsg;
         }
-        
+
         StringBuffer buffer = new StringBuffer();
         //generic exception info
         buffer.append("\nException class: ").append(errClass);
         buffer.append("\nMessage: ").append(errMsg);
-        if (err instanceof ErrorCoded) {
-            String errorCode = ((ErrorCoded) err).getErrorCode();
-            buffer.append("\nErrorCode: ").append(errorCode);
-            this.lblCode.setLabel(errorCode);
-        }
         buffer.append("\nStatusCode: ").append(errStatusCode);
         buffer.append("\nServletName: ").append(errServletName);
         buffer.append("\nReqURI: ").append(errReqURI);
-        
+
         Map<String, Object> browserInfo = ExecutionContext.getPage().getBrowserInfo();
         buffer.append(browserInfo);
-        
+
         buffer.append("\nThrowableContext: " + throwableContext);
         buffer.append("\nStackTrace: ");
         appendStackTrace(err);
-        
+
         log.error(buffer, err);
         this.lblExceptionClass.setLabel(String.valueOf(errClass));
         this.lblMessage.setLabel(errMsg);
         this.lblStatusCode.setLabel(String.valueOf(errStatusCode));
-        
+
         if (SecurityUtil.isGrantedAny(StrUtil.getLabel("cwf.error.dialog.expanded"))) {
             EventUtil.post(ClickEvent.TYPE, this.btnDetail, null);
         }
     }
-    
+
     /**
      * Appends the stack trace for the specified exception to the display.
-     * 
+     *
      * @param err Exception whose stack trace will be appended.
      */
     private void appendStackTrace(Throwable err) {
@@ -161,34 +151,34 @@ public class ExceptionController implements IAutoWired {
             Class<?> clazz = err.getClass();
             String msg = err.getMessage();
             //Throwable cause = err.getCause();//should be null
-            
+
             this.txtStackTrace.setValue(StringUtils.defaultString(this.txtStackTrace.getValue())
                     + StringUtils.trimToEmpty(clazz.getCanonicalName()) + ": " + StringUtils.trimToEmpty(msg) + "\n");
-            
+
             for (StackTraceElement element : err.getStackTrace()) {
                 this.txtStackTrace
                         .setValue(StringUtils.defaultString(this.txtStackTrace.getValue()) + String.valueOf(element) + "\n");
             }
         }
     }
-    
+
     /**
      * Sets the detail open state.
-     * 
+     *
      * @param doOpen The detail open state.
      */
     private void setDetail(boolean doOpen) {
         this.detail.setVisible(doOpen);
         this.btnDetail.setLabel(doOpen ? "Hide Detail" : "Show Detail");
     }
-    
+
     /**
      * Event handler for close button
      */
     public void onClick$btnClose() {
         this.root.detach();
     }
-    
+
     /**
      * Event handler for "show detail" button
      */
--- org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/util/TreeUtil.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/util/TreeUtil.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -35,38 +35,38 @@ import org.carewebframework.web.component.Treeview;
  * Useful tree functions.
  */
 public class TreeUtil {
-    
+
     /**
      * Interface for tree item search.
      */
     public interface ITreenodeSearch {
-        
+
         /**
          * Returns true if item matches the search text.
-         * 
+         *
          * @param item Tree item to inspect.
          * @param text Search text.
          * @return True if considered a match.
          */
         boolean isMatch(Treenode item, String text);
     }
-    
+
     /**
      * Default logic for tree item search.
      */
     private static final ITreenodeSearch defaultTreenodeSearch = new ITreenodeSearch() {
-        
+
         @Override
         public boolean isMatch(Treenode item, String text) {
             String label = item.getLabel();
             return label != null && label.toLowerCase().contains(text.toLowerCase());
         }
-        
+
     };
-    
+
     /**
      * Returns the tree item associated with the specified \-delimited path.
-     * 
+     *
      * @param tree Tree to search.
      * @param path \-delimited path to search. Search is not case sensitive.
      * @param create If true, tree nodes are created if they do not already exist.
@@ -76,10 +76,10 @@ public class TreeUtil {
     public static Treenode findNode(Treeview tree, String path, boolean create, Class<? extends Treenode> clazz) {
         return findNode(tree, path, create, clazz, MatchMode.CASE_INSENSITIVE);
     }
-    
+
     /**
      * Returns the tree item associated with the specified \-delimited path.
-     * 
+     *
      * @param tree Tree to search.
      * @param path \-delimited path to search.
      * @param create If true, tree nodes are created if they do not already exist.
@@ -92,13 +92,13 @@ public class TreeUtil {
         if (tree.getChildCount() == 0 && !create) {
             return null;
         }
-        
+
         return CWFUtil.findNode(tree, clazz, path, create, matchMode);
     }
-    
+
     /**
      * Returns the tree item associated with the specified \-delimited path.
-     * 
+     *
      * @param tree Tree to search. Search is not case sensitive.
      * @param path \-delimited path to search.
      * @param create If true, tree nodes are created if they do not already exist.
@@ -107,10 +107,10 @@ public class TreeUtil {
     public static Treenode findNode(Treeview tree, String path, boolean create) {
         return findNode(tree, path, create, Treenode.class);
     }
-    
+
     /**
      * Returns the tree item associated with the specified \-delimited path.
-     * 
+     *
      * @param tree Tree to search.
      * @param path \-delimited path to search.
      * @param create If true, tree nodes are created if they do not already exist.
@@ -120,11 +120,11 @@ public class TreeUtil {
     public static Treenode findNode(Treeview tree, String path, boolean create, MatchMode matchMode) {
         return findNode(tree, path, create, Treenode.class, matchMode);
     }
-    
+
     /**
      * Finds the node (tree item) with the specified label under the specified parent. If no node
      * matches the label, one will be created.
-     * 
+     *
      * @param parent Tree node whose children are to be searched.
      * @param label Label being sought (case-insensitive).
      * @param create If true and the matching tree item is not found, it will be created.
@@ -135,17 +135,17 @@ public class TreeUtil {
     public static Treenode findNodeByLabel(Treenode parent, String label, boolean create, Class<? extends Treenode> clazz,
                                            boolean caseSensitive) {
         Treenode item = null;
-        
+
         for (BaseComponent comp : parent.getChildren()) {
             if (comp instanceof Treenode) {
                 item = (Treenode) comp;
-                
+
                 if (caseSensitive ? item.getLabel().equals(label) : item.getLabel().equalsIgnoreCase(label)) {
                     return item;
                 }
             }
         }
-        
+
         if (create) {
             try {
                 item = clazz.newInstance();
@@ -157,13 +157,13 @@ public class TreeUtil {
                 throw MiscUtil.toUnchecked(e);
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Search the entire tree for a tree item matching the specified label.
-     * 
+     *
      * @param tree Tree containing the item of interest.
      * @param label Label to match.
      * @param caseSensitive If true, match is case-sensitive.
@@ -175,14 +175,14 @@ public class TreeUtil {
                 return item;
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Returns the path of the specified tree node. This consists of the indexes or labels of this
      * and all parent nodes separated by a "\" character.
-     * 
+     *
      * @param item The node (tree item) whose path is to be returned. If this value is null, a zero
      *            length string is returned.
      * @param useLabels If true, use the labels as identifiers; otherwise, use indexes.
@@ -191,33 +191,33 @@ public class TreeUtil {
     public static String getPath(Treenode item, boolean useLabels) {
         StringBuilder sb = new StringBuilder();
         boolean needsDelim = false;
-        
+
         while (item != null) {
             if (needsDelim) {
                 sb.insert(0, '\\');
             } else {
                 needsDelim = true;
             }
-            
+
             sb.insert(0, useLabels ? item.getLabel() : item.getIndex());
             item = (Treenode) item.getParent();
         }
-        
+
         return sb.toString();
     }
-    
+
     /**
      * Sorts all nodes in a tree alphabetically.
-     * 
+     *
      * @param tree Tree to be sorted.
      */
     public static void sort(Treeview tree) {
         sort(tree, true);
     }
-    
+
     /**
      * Alphabetically sorts children under the specified parent.
-     * 
+     *
      * @param parent Parent whose child nodes (Treenode) are to be sorted.
      * @param recurse If true, sorting is recursed through all children.
      */
@@ -225,14 +225,14 @@ public class TreeUtil {
         if (parent == null || parent.getChildren().size() < 2) {
             return;
         }
-        
+
         int i = 1;
         int size = parent.getChildren().size();
-        
+
         while (i < size) {
             Treenode item1 = (Treenode) parent.getChildren().get(i - 1);
             Treenode item2 = (Treenode) parent.getChildren().get(i);
-            
+
             if (compare(item1, item2) > 0) {
                 parent.swapChildren(i - 1, i);
                 i = i == 1 ? 2 : i - 1;
@@ -240,17 +240,17 @@ public class TreeUtil {
                 i++;
             }
         }
-        
+
         if (recurse) {
             for (BaseComponent child : parent.getChildren()) {
                 sort(child, recurse);
             }
         }
     }
-    
+
     /**
      * Case insensitive comparison of labels of two tree items.
-     * 
+     *
      * @param item1 First tree item.
      * @param item2 Second tree item.
      * @return Result of the comparison.
@@ -260,20 +260,20 @@ public class TreeUtil {
         String label2 = item2.getLabel();
         return label1 == label2 ? 0 : label1 == null ? -1 : label2 == null ? -1 : label1.compareToIgnoreCase(label2);
     }
-    
+
     /**
      * Expand / collapse a tree to the specified depth.
-     * 
+     *
      * @param tree Tree whose tree items are to be expanded / collapsed.
      * @param depth Expand tree items to this depth. Tree items below this depth are collapsed.
      */
     public static void expand(Treeview tree, int depth) {
         expandChildren(tree, depth);
     }
-    
+
     /**
      * Expand / collapse tree children to specified depth.
-     * 
+     *
      * @param parent Tree children whose tree items are to be expanded / collapsed.
      * @param depth Expand tree items to this depth. Tree items below this depth are collapsed.
      */
@@ -281,19 +281,19 @@ public class TreeUtil {
         if (parent == null || depth <= 0) {
             return;
         }
-        
+
         depth--;
-        
+
         for (Object object : parent.getChildren()) {
             Treenode item = (Treenode) object;
             item.setCollapsed(depth <= 0);
             expandChildren(item, depth);
         }
     }
-    
+
     /**
      * Search the tree for a tree item whose label contains the specified text.
-     * 
+     *
      * @param tree Tree to search.
      * @param text Text to find.
      * @return The first matching tree item, or null if none found.
@@ -301,10 +301,10 @@ public class TreeUtil {
     public static Treenode search(Treeview tree, String text) {
         return search(tree, text, defaultTreenodeSearch);
     }
-    
+
     /**
      * Search the tree for a tree item whose label contains the specified text.
-     * 
+     *
      * @param start Tree item to start search.
      * @param text Text to find.
      * @return The first matching tree item after the starting item, or null if none found.
@@ -312,10 +312,10 @@ public class TreeUtil {
     public static Treenode search(Treenode start, String text) {
         return search(start.getTreeview(), text, defaultTreenodeSearch);
     }
-    
+
     /**
      * Search the tree for a tree item whose label contains the specified text.
-     * 
+     *
      * @param start Tree item to start search.
      * @param text Text to find.
      * @param search Search logic.
@@ -324,11 +324,11 @@ public class TreeUtil {
     public static Treenode search(Treenode start, String text, ITreenodeSearch search) {
         return search(start.getTreeview(), text, search);
     }
-    
+
     /**
-     * Search the tree for a tree item whose label contains the specified text.
-     * 
-     * @param tree Tree to search.
+     * Search the tree for a tree node whose label contains the specified text.
+     *
+     * @param root Node where search should begin.
      * @param text Text to find.
      * @param search Search logic.
      * @return The first matching tree item after the last item, or null if none found.
@@ -341,24 +341,24 @@ public class TreeUtil {
         }
         return null;
     }
-    
+
     /**
      * Recursively adjusts the visibility of tree expand/collapse icons based on the visibility of
      * the child nodes.
-     * 
+     *
      * @param nodes Treenode components
      */
     /*TODO:
     public static void adjustVisibility(Iterable<Treenode> nodes) {
         if (nodes != null) {
             boolean visibleChildren = parent.getVisibleItemCount() > 0;
-            
+
             if (visibleChildren) {
                 for (BaseComponent item : parent.getChildren()) {
                     adjustVisibility(((Treenode) item).getTreechildren());
                 }
             }
-            
+
             if (parent.getParent() instanceof Treenode) {
                 adjustVisibility(parent, visibleChildren);
                 adjustVisibility((Treenode) parent.getParent(), visibleChildren);
@@ -367,11 +367,11 @@ public class TreeUtil {
         }
     }
     */
-    
+
     /**
      * Enforces static class.
      */
     private TreeUtil() {
     };
-    
+
 }
