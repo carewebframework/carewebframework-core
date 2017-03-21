117ddffb5565b337e6993b74717546eed789961e
Douglas Martin <dkmartin@regenstrief.org>
2017-03-21 11:51:28 -0400

    Highlight own session.

--- org.carewebframework.plugin-parent/org.carewebframework.plugin.sessiontracker/src/main/java/org/carewebframework/plugin/sessiontracker/SessionRenderer.java
+++ org.carewebframework.plugin-parent/org.carewebframework.plugin.sessiontracker/src/main/java/org/carewebframework/plugin/sessiontracker/SessionRenderer.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -27,6 +27,7 @@ package org.carewebframework.plugin.sessiontracker;
 
 import java.util.Date;
 
+import org.carewebframework.web.client.ExecutionContext;
 import org.carewebframework.web.component.Cell;
 import org.carewebframework.web.component.Row;
 import org.carewebframework.web.model.IComponentRenderer;
@@ -49,6 +50,11 @@ public class SessionRenderer implements IComponentRenderer<Row, Session> {
         createCell(row, clientAddress);
         createCell(row, creationTime);
         createCell(row, lastAccessedTime);
+        
+        if (session == ExecutionContext.getSession()) {
+            row.setStyles("font-weight:bold;color:blue");
+        }
+        
         return row;
     }
     
