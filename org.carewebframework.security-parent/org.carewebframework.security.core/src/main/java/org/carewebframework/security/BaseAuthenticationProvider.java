7510546b45fe0d64476bceaf1890010a46308101
Douglas Martin <dkmartin@regenstrief.org>
2017-04-06 13:14:47 -0400

    Update JavaDoc.

--- org.carewebframework.security-parent/org.carewebframework.security.core/src/main/java/org/carewebframework/security/BaseAuthenticationProvider.java
+++ org.carewebframework.security-parent/org.carewebframework.security.core/src/main/java/org/carewebframework/security/BaseAuthenticationProvider.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -32,11 +32,9 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.carewebframework.api.domain.IUser;
 import org.carewebframework.api.security.ISecurityDomain;
 import org.carewebframework.api.security.SecurityDomainRegistry;
-
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -79,11 +77,12 @@ public class BaseAuthenticationProvider implements AuthenticationProvider {
     }
     
     /**
-     * Authentication Provider. Produces a trusted <code>UsernamePasswordAuthenticationToken</code>
-     * if
-     * 
+     * Produces a trusted <code>UsernamePasswordAuthenticationToken</code> if authentication was
+     * successful.
+     *
      * @param authentication The authentication context.
-     * @return authentication Authentication object if authentication succeeded. Null if not.
+     * @return authentication Authentication object if authentication succeeded.
+     * @throws AuthenticationException Exception on authentication failure.
      */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -131,13 +130,13 @@ public class BaseAuthenticationProvider implements AuthenticationProvider {
     /**
      * Merges authorities granted (or revoked) by the system with those granted by the security
      * domain.
-     * 
+     *
      * @param userGrants Authorities granted by the security domain.
      * @param systemGrants Authorities granted by the system.
      * @return A merged set of granted authorities.
      */
     protected Set<String> mergeAuthorities(List<String> userGrants, List<String> systemGrants) {
-        Set<String> authorities = userGrants == null ? new HashSet<String>() : new HashSet<String>(userGrants);
+        Set<String> authorities = userGrants == null ? new HashSet<>() : new HashSet<>(userGrants);
         
         for (String grantedAuthority : systemGrantedAuthorities) {
             if (grantedAuthority.startsWith("-")) {
