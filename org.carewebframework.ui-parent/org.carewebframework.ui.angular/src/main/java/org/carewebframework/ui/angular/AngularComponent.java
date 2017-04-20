75d4d9c5f0391ed1c6b14f9b727f8f0d97b3c5d7
Douglas Martin <dkmartin@regenstrief.org>
2017-04-20 13:00:36 -0400

    Javadoc updates.

--- org.carewebframework.ui-parent/org.carewebframework.ui.angular/src/main/java/org/carewebframework/ui/angular/AngularComponent.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.angular/src/main/java/org/carewebframework/ui/angular/AngularComponent.java
@@ -1,3 +1,28 @@
+/*
+ * #%L
+ * carewebframework
+ * %%
+ * Copyright (C) 2008 - 2016 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This Source Code Form is also subject to the terms of the Health-Related
+ * Additional Disclaimer of Warranty and Limitation of Liability available at
+ *
+ *      http://www.carewebframework.org/licensing/disclaimer.
+ *
+ * #L%
+ */
 package org.carewebframework.ui.angular;
 
 import java.io.IOException;
@@ -6,33 +31,36 @@ import org.apache.commons.lang.StringUtils;
 import org.zkoss.zk.ui.HtmlBasedComponent;
 import org.zkoss.zk.ui.sys.ContentRenderer;
 
+/**
+ * Container for hosting an Angular 2 component.
+ */
 public class AngularComponent extends HtmlBasedComponent {
-    
+
     private static final long serialVersionUID = 1L;
-
+    
     private String src;
-
+    
     @Override
     public void renderProperties(ContentRenderer renderer) throws IOException {
         super.renderProperties(renderer);
         renderer.render("src", src);
     }
-
+    
     @Override
     public String getZclass() {
         return _zclass == null ? "cwf-angular" : _zclass;
     }
-
+    
     public String getSrc() {
         return src;
     }
-
+    
     public void setSrc(String src) {
         src = StringUtils.trimToNull(src);
-        
+
         if (!StringUtils.equals(src, this.src)) {
             smartUpdate("src", this.src = src);
         }
     }
-
+    
 }
