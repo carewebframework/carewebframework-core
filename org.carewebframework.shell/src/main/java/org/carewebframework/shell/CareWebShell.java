80891a10ccfaa8af9c08ab8032be45ee1cf72257
Douglas Martin <dkmartin@regenstrief.org>
2017-05-26 14:46:11 -0400

    Shell now implements INamespace.

--- org.carewebframework.shell/src/main/java/org/carewebframework/shell/CareWebShell.java
+++ org.carewebframework.shell/src/main/java/org/carewebframework/shell/CareWebShell.java
@@ -61,6 +61,7 @@ import org.carewebframework.ui.command.CommandRegistry;
 import org.carewebframework.ui.command.CommandUtil;
 import org.carewebframework.ui.dialog.DialogUtil;
 import org.carewebframework.ui.session.SessionControl;
+import org.carewebframework.web.ancillary.INamespace;
 import org.carewebframework.web.annotation.Component;
 import org.carewebframework.web.annotation.Component.ChildTag;
 import org.carewebframework.web.annotation.Component.PropertyGetter;
@@ -79,57 +80,57 @@ import org.carewebframework.web.event.KeycaptureEvent;
  * Implements a generic UI shell that can be dynamically extended with plug-ins.
  */
 @Component(value = "cwfShell", widgetClass = "Div", parentTag = "*", childTag = @ChildTag("*"))
-public class CareWebShell extends Div {
-    
-    protected static final Log log = LogFactory.getLog(CareWebShell.class);
-    
-    public final String LBL_NO_LAYOUT = StrUtil.getLabel("cwf.shell.nolayout.message");
-    
-    public final String LBL_LOGOUT_CONFIRMATION = StrUtil.getLabel("cwf.shell.logout.confirmation.message");
-    
-    public final String LBL_LOGOUT_CONFIRMATION_CAPTION = StrUtil.getLabel("cwf.shell.logout.confirmation.caption");
-    
-    public final String LBL_LOGOUT_CANCEL = StrUtil.getLabel("cwf.shell.logout.cancel.message");
-    
-    private final AppFramework appFramework = FrameworkUtil.getAppFramework();
-    
-    private final IEventManager eventManager = EventManager.getInstance();
-    
-    private final CommandRegistry commandRegistry = SpringUtil.getBean("commandRegistry", CommandRegistry.class);
-    
-    private final List<UIElementPlugin> plugins = new ArrayList<>();
-    
-    private final Set<HelpModule> helpModules = new HashSet<>();
-    
-    private final Set<IHelpSet> helpSets = new HashSet<>();
-    
-    private final List<String> propertyGroups = new ArrayList<>();
-    
-    private UILayout layout = new UILayout();
-    
-    private UIElementDesktop desktop;
-    
-    private final BaseComponent registeredStyles = new Span();
-    
-    private CareWebStartup startupRoutines;
-    
-    private MessageWindow messageWindow;
-    
-    private String defaultLayoutName;
-    
-    private boolean autoStart;
+public class CareWebShell extends Div implements INamespace {
 
-    private boolean logoutConfirm = true;
+    protected static final Log log = LogFactory.getLog(CareWebShell.class);
+
+    public final String LBL_NO_LAYOUT = StrUtil.getLabel("cwf.shell.nolayout.message");
+
+    public final String LBL_LOGOUT_CONFIRMATION = StrUtil.getLabel("cwf.shell.logout.confirmation.message");
+
+    public final String LBL_LOGOUT_CONFIRMATION_CAPTION = StrUtil.getLabel("cwf.shell.logout.confirmation.caption");
+
+    public final String LBL_LOGOUT_CANCEL = StrUtil.getLabel("cwf.shell.logout.cancel.message");
+
+    private final AppFramework appFramework = FrameworkUtil.getAppFramework();
+
+    private final IEventManager eventManager = EventManager.getInstance();
+
+    private final CommandRegistry commandRegistry = SpringUtil.getBean("commandRegistry", CommandRegistry.class);
+
+    private final List<UIElementPlugin> plugins = new ArrayList<>();
+
+    private final Set<HelpModule> helpModules = new HashSet<>();
+
+    private final Set<IHelpSet> helpSets = new HashSet<>();
+
+    private final List<String> propertyGroups = new ArrayList<>();
+
+    private UILayout layout = new UILayout();
+
+    private UIElementDesktop desktop;
+
+    private final BaseComponent registeredStyles = new Span();
+
+    private CareWebStartup startupRoutines;
+
+    private MessageWindow messageWindow;
+
+    private String defaultLayoutName;
+
+    private boolean autoStart;
     
+    private boolean logoutConfirm = true;
+
     private final IUserContextEvent userContextListener = new IUserContextEvent() {
-        
+
         /**
          * @see IUserContextEvent#canceled()
          */
         @Override
         public void canceled() {
         }
-        
+
         /**
          * @see IUserContextEvent#committed()
          */
@@ -137,7 +138,7 @@ public class CareWebShell extends Div {
         public void committed() {
             reset();
         }
-        
+
         /**
          * Prompt user for logout confirmation (unless suppressed).
          *
@@ -149,20 +150,20 @@ public class CareWebShell extends Div {
                 response.accept();
             } else {
                 response.defer();
-                
+
                 DialogUtil.confirm(LBL_LOGOUT_CONFIRMATION, LBL_LOGOUT_CONFIRMATION_CAPTION, "LOGOUT.CONFIRM", (confirm) -> {
                     if (confirm) {
                         response.accept();
                     } else {
                         response.reject(LBL_LOGOUT_CANCEL);
                     }
-                    
+
                 });
             }
         }
-        
+
     };
-    
+
     /**
      * Returns the application name for this instance of the CareWeb shell.
      *
@@ -171,7 +172,7 @@ public class CareWebShell extends Div {
     public static String getApplicationName() {
         return FrameworkUtil.getAppName();
     }
-    
+
     /**
      * Create the shell instance.
      */
@@ -179,7 +180,7 @@ public class CareWebShell extends Div {
         super();
         CareWebUtil.setShell(this);
     }
-    
+
     @Override
     protected void onAttach(Page page) {
         try {
@@ -189,24 +190,24 @@ public class CareWebShell extends Div {
             desktop = new UIElementDesktop(this);
             setLogoutConfirm(logoutConfirm);
             String confirmClose = getAppProperty("confirmClose", "CAREWEB.CONFIRM.CLOSE");
-            
+
             if (StringUtils.isEmpty(confirmClose) || BooleanUtils.toBoolean(confirmClose)) {
                 ClientUtil.canClose(false);
             }
-            
+
             String layout = defaultLayoutName != null ? defaultLayoutName
                     : getAppProperty("layout", "CAREWEB.LAYOUT.DEFAULT");
-            
+
             if (!StringUtils.isEmpty(layout)) {
                 loadLayout(layout);
             }
-            
+
         } catch (Exception e) {
             log.error("Error initializing the shell.", e);
             throw MiscUtil.toUnchecked(e);
         }
     }
-    
+
     /**
      * Handle help requests.
      *
@@ -218,7 +219,7 @@ public class CareWebShell extends Div {
             HelpUtil.showCSH(ref == null ? event.getTarget() : ref);
         }
     }
-    
+
     /**
      * Capture unhandled shortcut key press events.
      *
@@ -226,12 +227,12 @@ public class CareWebShell extends Div {
      */
     public void onKeycapture(KeycaptureEvent event) {
         String shortcut = event.getKeycapture();
-        
+
         for (UIElementPlugin plugin : getActivatedPlugins(null)) {
             commandRegistry.fireCommands(shortcut, event, plugin.getOuterComponent());
         }
     }
-    
+
     /**
      * Returns a reference to the current UI desktop.
      *
@@ -240,7 +241,7 @@ public class CareWebShell extends Div {
     public UIElementDesktop getUIDesktop() {
         return desktop;
     }
-    
+
     /**
      * Returns a reference to the current UI layout.
      *
@@ -249,14 +250,14 @@ public class CareWebShell extends Div {
     public UILayout getUILayout() {
         return layout;
     }
-    
+
     /**
      * Executed once all plugins are loaded.
      */
     public void start() {
         desktop.activate(true);
         String initialPlugin = PropertyUtil.getValue("CAREWEB.INITIAL.SECTION", getApplicationName());
-        
+
         if (!StringUtils.isEmpty(initialPlugin)) {
             for (UIElementPlugin plugin : plugins) {
                 if (initialPlugin.equals(plugin.getDefinition().getId())) {
@@ -265,14 +266,14 @@ public class CareWebShell extends Div {
                 }
             }
         }
-        
+
         if (startupRoutines == null) {
             startupRoutines = SpringUtil.getBean("careWebStartup", CareWebStartup.class);
         }
-        
+
         startupRoutines.execute();
     }
-    
+
     /**
      * Loads a layout from the specified resource.
      *
@@ -282,14 +283,14 @@ public class CareWebShell extends Div {
     public void loadLayout(String resource) throws Exception {
         layout = UILayout.load(resource);
         FrameworkUtil.setAppName(layout.getName());
-        
+
         if (layout.isEmpty()) {
             DialogUtil.showError(LBL_NO_LAYOUT);
         } else {
             buildUI(layout);
         }
     }
-    
+
     /**
      * Returns the name of the layout to be loaded.
      *
@@ -299,7 +300,7 @@ public class CareWebShell extends Div {
     public String getLayout() {
         return defaultLayoutName;
     }
-    
+
     /**
      * Sets the layout to be loaded. If null, the layout specified by the configuration will be
      * loaded.
@@ -310,12 +311,12 @@ public class CareWebShell extends Div {
     @PropertySetter("layout")
     public void setLayout(String defaultLayoutName) throws Exception {
         this.defaultLayoutName = defaultLayoutName;
-        
+
         if (desktop != null && !StringUtils.isEmpty(defaultLayoutName)) {
             loadLayout(defaultLayoutName);
         }
     }
-    
+
     /**
      * Returns the auto-start setting.
      *
@@ -325,7 +326,7 @@ public class CareWebShell extends Div {
     public boolean isAutoStart() {
         return autoStart;
     }
-    
+
     /**
      * Sets the auto-start setting.
      *
@@ -335,7 +336,7 @@ public class CareWebShell extends Div {
     public void setAutoStart(boolean autoStart) {
         this.autoStart = autoStart;
     }
-    
+
     /**
      * Returns true if confirmation is required upon user-initiated logout.
      *
@@ -344,7 +345,7 @@ public class CareWebShell extends Div {
     public boolean isLogoutConfirm() {
         return logoutConfirm;
     }
-    
+
     /**
      * Set to true if confirmation is required upon user-initiated logout.
      *
@@ -352,14 +353,14 @@ public class CareWebShell extends Div {
      */
     public void setLogoutConfirm(boolean logoutConfirm) {
         this.logoutConfirm = logoutConfirm;
-
+        
         if (logoutConfirm) {
             appFramework.registerObject(userContextListener);
         } else {
             appFramework.unregisterObject(userContextListener);
         }
     }
-    
+
     /**
      * Build the UI based on the specified layout.
      *
@@ -374,19 +375,19 @@ public class CareWebShell extends Div {
         desktop.setIcon(layout.readString("icon", ""));
         desktop.setAppId(FrameworkUtil.getAppName());
         desktop.activate(true);
-        
+
         if (autoStart) {
             start();
         }
     }
-    
+
     /**
      * Resets the desktop to its baseline state and clears registered help modules and property
      * groups.
      */
     public void reset() {
         FrameworkUtil.setAppName(null);
-        
+
         try {
             desktop.activate(false);
             desktop.clear();
@@ -400,7 +401,7 @@ public class CareWebShell extends Div {
             plugins.clear();
         } catch (Exception e) {}
     }
-    
+
     /**
      * Registers a plugin and its resources. Called internally when a plugin is instantiated.
      *
@@ -409,7 +410,7 @@ public class CareWebShell extends Div {
     public void registerPlugin(UIElementPlugin plugin) {
         plugins.add(plugin);
     }
-    
+
     /**
      * Unregisters a plugin and its resources. Called internally when a plugin is destroyed.
      *
@@ -418,7 +419,7 @@ public class CareWebShell extends Div {
     public void unregisterPlugin(UIElementPlugin plugin) {
         plugins.remove(plugin);
     }
-    
+
     /**
      * Adds a component to the common tool bar.
      *
@@ -427,7 +428,7 @@ public class CareWebShell extends Div {
     public void addToolbarComponent(BaseComponent component) {
         desktop.getToolbar().addToolbarComponent(component, null);
     }
-    
+
     /**
      * Registers a help resource.
      *
@@ -435,23 +436,23 @@ public class CareWebShell extends Div {
      */
     public void registerHelpResource(PluginResourceHelp resource) {
         HelpModule def = HelpModule.getModule(resource.getModule());
-        
+
         if (def != null && helpModules.add(def)) {
             IHelpSet hs = HelpSetCache.getInstance().get(def);
-            
+
             if (hs != null) {
                 helpSets.add(hs);
                 IHelpViewer viewer = HelpUtil.getViewer(false);
-                
+
                 if (viewer != null) {
                     viewer.mergeHelpSet(hs);
                 }
             }
         }
-        
+
         desktop.addHelpMenu(resource);
     }
-    
+
     /**
      * Registers an external style sheet. If the style sheet has not already been registered,
      * creates a style component and adds it to the current page.
@@ -465,7 +466,7 @@ public class CareWebShell extends Div {
             registeredStyles.addChild(ss);
         }
     }
-    
+
     /**
      * Returns the style sheet associated with the specified URL.
      *
@@ -478,10 +479,10 @@ public class CareWebShell extends Div {
                 return ss;
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Registers a property group.
      *
@@ -493,7 +494,7 @@ public class CareWebShell extends Div {
             eventManager.fireLocalEvent(Constants.EVENT_RESOURCE_PROPGROUP_ADD, propertyGroup);
         }
     }
-    
+
     /**
      * Adds a menu.
      *
@@ -504,7 +505,7 @@ public class CareWebShell extends Div {
     public BaseMenuComponent addMenu(String path, String action) {
         return desktop.addMenu(path, action, false);
     }
-    
+
     /**
      * Returns a list of all plugins currently loaded into the UI.
      *
@@ -513,7 +514,7 @@ public class CareWebShell extends Div {
     public Iterable<UIElementPlugin> getLoadedPlugins() {
         return plugins;
     }
-    
+
     /**
      * Locates a loaded plugin with the specified id.
      *
@@ -526,10 +527,10 @@ public class CareWebShell extends Div {
                 return plugin;
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Locates a loaded plugin with the specified id.
      *
@@ -539,14 +540,14 @@ public class CareWebShell extends Div {
      */
     public UIElementPlugin getLoadedPlugin(String id, boolean forceInit) {
         UIElementPlugin plugin = getLoadedPlugin(id);
-        
+
         if (plugin != null && forceInit) {
             plugin.load();
         }
-        
+
         return plugin;
     }
-    
+
     /**
      * Locates an activated plugin with the specified id.
      *
@@ -559,10 +560,10 @@ public class CareWebShell extends Div {
                 return plugin;
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Returns a list of all active plugins.
      *
@@ -571,7 +572,7 @@ public class CareWebShell extends Div {
     public Iterable<UIElementPlugin> getActivatedPlugins() {
         return getActivatedPlugins(null);
     }
-    
+
     /**
      * Populates a list of all activated plugins.
      *
@@ -584,16 +585,16 @@ public class CareWebShell extends Div {
         } else {
             list.clear();
         }
-        
+
         for (UIElementPlugin plugin : plugins) {
             if (plugin.isActivated()) {
                 list.add(plugin);
             }
         }
-        
+
         return list;
     }
-    
+
     /**
      * Returns a list of all plugin definitions that are currently in use (i.e., have associated
      * plugins loaded) in the environment.
@@ -603,18 +604,18 @@ public class CareWebShell extends Div {
      */
     public Iterable<PluginDefinition> getLoadedPluginDefinitions() {
         List<PluginDefinition> result = new ArrayList<>();
-        
+
         for (UIElementPlugin plugin : plugins) {
             PluginDefinition def = plugin.getDefinition();
-            
+
             if (!result.contains(def)) {
                 result.add(def);
             }
         }
-        
+
         return result;
     }
-    
+
     /**
      * Returns a list of property groups bound to loaded plugins. Guarantees each group name will
      * appear at most once in the list.
@@ -624,34 +625,34 @@ public class CareWebShell extends Div {
     public List<String> getPropertyGroups() {
         return propertyGroups;
     }
-    
+
     public String getAppProperty(String queryParam, String propName) {
         String result = getPage().getQueryParam(queryParam);
         return result == null ? PropertyUtil.getValue(propName) : result;
     }
-    
+
     /**
      * Logout user after confirmation prompt.
      */
     public void logout() {
         // Ensure that shell is last context subscriber (should be a better way
         // to do this).
-
+        
         if (logoutConfirm) {
             setLogoutConfirm(false);
             setLogoutConfirm(true);
         }
-
+        
         SecurityUtil.getSecurityService().logout(false, null, null);
     }
-    
+
     /**
      * Lock the desktop.
      */
     public void lock() {
         eventManager.fireLocalEvent(SessionControl.LOCK.getEventName(), true);
     }
-    
+
     /**
      * Returns the message window instance for managing slide-down messages.
      *
@@ -660,7 +661,7 @@ public class CareWebShell extends Div {
     public MessageWindow getMessageWindow() {
         return messageWindow;
     }
-    
+
     /**
      * Returns reference to the help viewer. If not already created, one will be created and
      * initialized with the registered help sets.
@@ -669,14 +670,14 @@ public class CareWebShell extends Div {
      */
     protected IHelpViewer getHelpViewer() {
         IHelpViewer viewer = HelpUtil.getViewer(false);
-        
+
         if (viewer != null) {
             return viewer;
         }
-        
+
         viewer = HelpUtil.getViewer(true);
         viewer.load(helpSets);
         return viewer;
     }
-    
+
 }
