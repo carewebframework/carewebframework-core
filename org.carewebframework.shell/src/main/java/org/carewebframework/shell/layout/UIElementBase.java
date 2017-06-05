7c790e6e85f7097bf24c11e15713f2cc193b3c0d
Douglas Martin <dkmartin@regenstrief.org>
2017-06-05 16:32:49 -0400

    Fix getVisibleChild

--- org.carewebframework.shell/src/main/java/org/carewebframework/shell/layout/UIElementBase.java
+++ org.carewebframework.shell/src/main/java/org/carewebframework/shell/layout/UIElementBase.java
@@ -764,11 +764,11 @@ public abstract class UIElementBase {
      * @return Visible child, or null if none found.
      */
     private UIElementBase getVisibleChild(boolean first) {
-        int start = first ? 0 : getChildCount() - 1;
-        int end = first ? getChildCount() - 1 : 0;
+        int count = getChildCount();
+        int start = first ? 0 : count - 1;
         int inc = first ? 1 : -1;
         
-        for (int i = start; i <= end; i += inc) {
+        for (int i = start; i >= 0 && i < count; i += inc) {
             if (getChild(i).isVisible()) {
                 return getChild(i);
             }
