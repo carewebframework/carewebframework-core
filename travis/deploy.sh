0c14d14eaee496bfa7f89a3e25cdc56b2d747dc5
Douglas Martin <dkmartin@regenstrief.org>
2017-07-07 14:09:28 -0400

    Add travis config

---
+++ travis/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  openssl aes-256-cbc -K $encrypted_784c08ba1d52_key -iv $encrypted_784c08ba1d52_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+  gpg --fast-import travis/codesigning.asc
+  mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
+fi
