aef4999467ca15afadf90233f9619119dc124faa
Douglas Martin <dkmartin@regenstrief.org>
2017-09-23 22:05:49 -0400

    Set Fujion version to 1.0.8

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = '6.0.x' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [[ "$TRAVIS_BRANCH" = 'master' ]] || [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.x$ ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
   openssl aes-256-cbc -K $encrypted_784c08ba1d52_key -iv $encrypted_784c08ba1d52_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
