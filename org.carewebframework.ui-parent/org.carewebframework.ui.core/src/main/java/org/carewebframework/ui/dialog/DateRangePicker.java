7130e154e4d3b67034ce0f94aef41dfd5cca755b
Douglas Martin <dkmartin@regenstrief.org>
2017-10-25 13:13:12 -0400

    DateRangePicker now correctly recognizes custom item.

--- org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/dialog/DateRangePicker.java
+++ org.carewebframework.ui-parent/org.carewebframework.ui.core/src/main/java/org/carewebframework/ui/dialog/DateRangePicker.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -30,12 +30,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.carewebframework.api.property.PropertyUtil;
-import org.fujion.common.DateRange;
 import org.fujion.annotation.Component;
 import org.fujion.annotation.Component.ChildTag;
 import org.fujion.annotation.Component.PropertyGetter;
 import org.fujion.annotation.Component.PropertySetter;
 import org.fujion.annotation.EventHandler;
+import org.fujion.common.DateRange;
 import org.fujion.component.BaseComponent;
 import org.fujion.component.Combobox;
 import org.fujion.component.Comboitem;
@@ -75,7 +75,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Load choices from the named property
-     * 
+     *
      * @param propertyName Name of the property containing the choices. Must be in the acceptable
      *            format. If the property name is null, loads a default set of choices.
      */
@@ -89,7 +89,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Load choices from a string array.
-     * 
+     *
      * @param choices A string array containing choices.
      */
     public void loadChoices(String... choices) {
@@ -98,7 +98,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Load choices from a list.
-     * 
+     *
      * @param choices A list of choices.
      */
     public void loadChoices(Iterable<String> choices) {
@@ -115,7 +115,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Adds a date range to the choice list.
-     * 
+     *
      * @param range Date range item
      * @param isCustom If true, range is a custom item. In this case, if another matching custom
      *            item exists, it will not be added.
@@ -147,7 +147,7 @@ public class DateRangePicker extends Combobox {
     /**
      * Adds a date range item to the choice list from its string representation (see DateRange class
      * for format).
-     * 
+     *
      * @param range String representation of date range.
      * @param isCustom If true, range is a custom item. In this case, if another matching custom
      *            item exists, it will not be added.
@@ -173,7 +173,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Searches for a comboitem that has a date range equivalent to the specified range.
-     * 
+     *
      * @param range The date range to locate.
      * @return A comboitem containing the date range, or null if not found.
      */
@@ -190,7 +190,7 @@ public class DateRangePicker extends Combobox {
     /**
      * Searches for a comboitem that has a label that matches the specified value. The search is
      * case insensitive.
-     * 
+     *
      * @param label Label text to find.
      * @return A comboitem with a matching label., or null if not found.
      */
@@ -208,7 +208,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Need to update visual appearance of selection when it is changed.
-     * 
+     *
      * @see org.fujion.component.Combobox#setSelectedItem(Comboitem)
      */
     @Override
@@ -221,7 +221,7 @@ public class DateRangePicker extends Combobox {
      * If set to true, the user may select "Custom..." from the choice list and enter a custom date
      * range. If set to false, this choice is hidden and any existing custom entries are removed
      * from the list.
-     * 
+     *
      * @param allowCustom Determines whether or not custom entries are allowed.
      */
     @PropertySetter("allowCustom")
@@ -239,7 +239,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Returns true if custom ranges are allowed.
-     * 
+     *
      * @return True if custom ranges are allowed.
      */
     @PropertyGetter("allowCustom")
@@ -249,7 +249,7 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Returns the date range item that is currently selected.
-     * 
+     *
      * @return Selected date range item, or null if none selected.
      */
     public DateRange getSelectedRange() {
@@ -260,7 +260,7 @@ public class DateRangePicker extends Combobox {
     /**
      * Returns the selected start date. This may be null if there is no active selection or if the
      * selected date range has no start date.
-     * 
+     *
      * @return Starting date of range, or null.
      */
     public Date getStartDate() {
@@ -271,7 +271,7 @@ public class DateRangePicker extends Combobox {
     /**
      * Returns the selected end date. This may be null if there is no active selection or if the
      * selected date range has no end date.
-     * 
+     *
      * @return Ending date of range, or null.
      */
     public Date getEndDate() {
@@ -282,7 +282,7 @@ public class DateRangePicker extends Combobox {
     /**
      * Check the current selection. If nothing is selected, display a prompt message in gray text.
      * Also, remembers the last selection made.
-     * 
+     *
      * @param suppressEvent If true, onSelectRange event is not fired.
      */
     private void checkSelection(boolean suppressEvent) {
@@ -324,13 +324,13 @@ public class DateRangePicker extends Combobox {
     
     /**
      * Change event handler.
-     * 
+     *
      * @param event The change event.
      */
     @EventHandler("change")
     private void onChange(ChangeEvent event) {
         // When the custom range item is selected, triggers the display of the date range dialog.
-        if (event.getTarget() == customItem) {
+        if (event.getRelatedTarget() == customItem) {
             event.stopPropagation();
             DateRangeDialog.show((range) -> {
                 setSelectedItem(range == null ? lastSelectedItem : addChoice(range, true));
