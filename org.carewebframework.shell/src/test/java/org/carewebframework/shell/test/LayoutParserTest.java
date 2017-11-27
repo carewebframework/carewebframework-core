e80ef5925d3be7ab364484a4696493149864320d
Douglas Martin <dkmartin@regenstrief.org>
2017-11-27 10:59:46 -0500

    Fix unit test.

--- org.carewebframework.shell/src/test/java/org/carewebframework/shell/test/LayoutParserTest.java
+++ org.carewebframework.shell/src/test/java/org/carewebframework/shell/test/LayoutParserTest.java
@@ -31,7 +31,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.fujion.common.StrUtil;
 import org.carewebframework.shell.CareWebShell;
 import org.carewebframework.shell.elements.ElementBase;
 import org.carewebframework.shell.elements.ElementDesktop;
@@ -54,17 +53,18 @@ import org.carewebframework.shell.property.PropertyInfo;
 import org.carewebframework.shell.triggers.TriggerConditionActivate;
 import org.carewebframework.ui.controller.FrameworkController;
 import org.carewebframework.ui.test.MockUITest;
+import org.fujion.common.StrUtil;
 import org.fujion.event.ClickEvent;
 import org.fujion.event.EventUtil;
 import org.fujion.test.MockTest;
 import org.junit.Test;
 
 public class LayoutParserTest extends MockUITest {
-
+    
     private CareWebShell shell;
-    
+
     private ElementUI element;
-    
+
     @Test
     public void parserTest() throws Exception {
         shell = new CareWebShell();
@@ -72,18 +72,18 @@ public class LayoutParserTest extends MockUITest {
         parserTestFile("layout-v3.xml", false);
         parserTestFile("layout-v4.xml", true);
     }
-
+    
     private void parserTestFile(String file, boolean hasTrigger) throws Exception {
         Layout layout = parserTestXML(getTextFromResource(file), hasTrigger);
         parserTestXML(layout.toString(), hasTrigger);
     }
-
+    
     private Layout parserTestXML(String xml, boolean hasTrigger) throws Exception {
         Layout layout = LayoutParser.parseText(xml);
         parserTestLayout(layout, hasTrigger);
         return layout;
     }
-
+    
     private void parserTestLayout(Layout layout, boolean hasTrigger) throws Exception {
         assertFalse(layout.isEmpty());
         assertEquals(layout.getName(), "test");
@@ -100,7 +100,7 @@ public class LayoutParserTest extends MockUITest {
         testNode(0, ElementToolbar.class);
         testNode(0, ElementSplitterView.class);
         testNode(1, ElementSplitterPane.class);
-        testProperty("size", 90);
+        testProperty("size", 90.0);
         testProperty("relative", true);
         testNode(1, ElementTabView.class);
         testProperty("orientation", "top");
@@ -115,13 +115,13 @@ public class LayoutParserTest extends MockUITest {
         assertNotNull(plugin1);
         assertEquals(hasTrigger ? 1 : 0, plugin1.getTriggers().size());
         assertEquals(hasTrigger ? "triggered" : null, plugin1.getHint());
-
+        
         if (hasTrigger) {
             ElementTrigger trigger = plugin1.getTriggers().iterator().next();
             assertTrue(trigger.getAction() instanceof TestTriggerAction);
             assertTrue(trigger.getCondition() instanceof TriggerConditionActivate);
         }
-
+        
         PluginContainer container1 = (PluginContainer) plugin1.getOuterComponent();
         TestPluginController controller = (TestPluginController) FrameworkController
                 .getController(container1.getFirstChild());
@@ -146,22 +146,22 @@ public class LayoutParserTest extends MockUITest {
         root.removeChildren();
         testPlugin(controller, 1, 2, 1, 1);
     }
-
+    
     private void testProperty(ElementPlugin plugin, String propertyName, Object expectedValue) throws Exception {
         PluginDefinition def = plugin.getDefinition();
         PropertyInfo propInfo = null;
-
+        
         for (PropertyInfo pi : def.getProperties()) {
             if (pi.getId().equals(propertyName)) {
                 propInfo = pi;
                 break;
             }
         }
-
+        
         assertNotNull("Property not found: " + propertyName, propInfo);
         assertEquals(expectedValue, plugin.getPropertyValue(propInfo));
     }
-
+    
     private void testPlugin(TestPluginController controller, int loadCount, int activateCount, int inactivateCount,
                             int unloadCount) {
         getMockEnvironment().flushEvents();
@@ -170,25 +170,25 @@ public class LayoutParserTest extends MockUITest {
         assertEquals(inactivateCount, controller.getInactivateCount());
         assertEquals(unloadCount, controller.getUnloadCount());
     }
-
+    
     private void testNode(int dir, Class<? extends ElementUI> clazz) {
         switch (dir) {
             case 1:
                 element = (ElementUI) element.getFirstChild();
                 break;
-
+            
             case -1:
                 element = element.getParent();
                 break;
-
+            
             case 0:
                 element = element.getNextSibling(false);
                 break;
         }
-
+        
         assertTrue(clazz.isInstance(element));
     }
-
+    
     private void testProperty(String key, Object value) throws Exception {
         Object prop = PropertyUtils.getProperty(element, key);
         assertEquals(prop, value);
