a224e2ea414baf2fbed19dfd91dc64b9c408395e
Douglas Martin <dkmartin@regenstrief.org>
2018-04-17 10:33:26 -0400

    H2 config updates.

--- org.carewebframework.hibernate-parent/org.carewebframework.hibernate.h2/src/main/java/org/carewebframework/hibernate/h2/H2DataSource.java
+++ org.carewebframework.hibernate-parent/org.carewebframework.hibernate.h2/src/main/java/org/carewebframework/hibernate/h2/H2DataSource.java
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related
  * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
@@ -28,55 +28,55 @@ package org.carewebframework.hibernate.h2;
 import java.sql.SQLException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-
-import org.springframework.util.StringUtils;
-
 import org.h2.tools.Server;
+import org.springframework.util.StringUtils;
 
 /**
  * H2-based data source that also handles starting the database in the appropriate mode.
  */
 public class H2DataSource extends BasicDataSource {
-    
-    
+
+    /**
+     * Server modes.
+     */
     public enum DBMode {
         EMBEDDED, // H2 embedded mode
         REMOTE, // H2 remote server
         LOCAL // H2 local server
-    };
-    
+    }
+
     private Server server;
-    
+
     private DBMode dbMode = DBMode.EMBEDDED;
-    
+
     public H2DataSource() {
     }
-    
+
     /**
      * If running H2 in local mode, starts the server.
-     * 
+     *
      * @return this (for chaining)
      * @throws Exception Unspecified exception
      */
     public H2DataSource init() throws Exception {
         if (dbMode == DBMode.LOCAL) {
             String port = getPort();
-            
+
             if (port.isEmpty()) {
                 server = Server.createTcpServer();
             } else {
                 server = Server.createTcpServer("-tcpPort", port);
             }
-            
+
             server.start();
         }
-        
+
         return this;
     }
-    
+
     /**
      * Extract the TCP port from the connection URL.
-     * 
+     *
      * @return The TCP port, or empty string if none.
      */
     private String getPort() {
@@ -87,26 +87,26 @@ public class H2DataSource extends BasicDataSource {
         i = s.indexOf(":");
         return i == -1 ? "" : s.substring(i + 1);
     }
-    
+
     public void destroy() throws Exception {
         close();
     }
-    
+
     public DBMode getMode() {
         return dbMode;
     }
-    
+
     public void setMode(String value) {
         dbMode = StringUtils.isEmpty(value) ? DBMode.EMBEDDED : DBMode.valueOf(value.toUpperCase());
     }
-    
+
     @Override
     public void close() throws SQLException {
         super.close();
-        
+
         if (server != null) {
             server.stop();
         }
     }
-    
+
 }
